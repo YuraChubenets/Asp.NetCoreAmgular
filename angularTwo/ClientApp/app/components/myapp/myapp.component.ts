06a9fcd9d83ab5a06bab3d49b476105296324736
DESKTOP-E56PFGU\yuriy <u.chubenets@gmail.com>
2017-06-07 20:40:22 +0300

    start project

---
+++ angularTwo/ClientApp/app/components/myapp/myapp.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'myapp',
+    templateUrl: './myapp.component.html',
+    styleUrls: ['./myapp.component.css']
+})
+export class AppComponent {
+}
